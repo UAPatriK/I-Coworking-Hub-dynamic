f5a2d4ce8ed22520e52cefc67301a82df3422066
UAPatriK <vladswls@gmail.com>
2017-03-07 21:27:39 +0200

    Team page

--- archive-team.php
+++ archive-team.php
@@ -1,210 +1,163 @@
 <?php get_header(); ?>
 
-
-       <div class="site-wrapper">
-      <div class="site-wrapper-inner">
-        <div class="cover-container">
-
-          <div class="" style="">
-           <img src="images/pm_course_page.png" class="img-responsive" alt="">
-          </div>
-
-      <div class="col-col-lg-11" style="padding-left:15px; margin-top:50px">  
-      <h1 class="course_sign">Команда<br>
-Team</h1></div>
-
-
-
-        </div>
-
+<!-- BLOCK WITH HEAD -->
+<div class="site-wrapper">
+  <div class="site-wrapper-inner"  style="vertical-align: top;">
+    <div class="cover-container" >
+      <img src="<?php bloginfo('template_url'); ?>/images/team_cover.jpg" class="img-cover" alt="">
+      <div class="col-col-lg-11" style="padding-left:45px; margin-top:50px; ">  
+        <h1 class="course_sign">Команда<br>
+        Team</h1>
       </div>
+    </div>
+  </div>
+</div>
+<!-- END OF BLOCK WITH HEAD -->
 
+
+<!-- BLOCK WITH TUTORS MENTORS OF DESIGN SCHOOL -->
+<div class="col-xs-12 block_borders slide-styles" >
+  <section class="main1">
+
+    <div class="col-sm-6 col-md-4 col-xs-12 text_block" >
+      <h1>Тьюторы, менторы <br>Школы Дизайна.<br>
+        Design School Tutors, mentors.</h1>
     </div>
 
-
-
-
-
-
-
-
-<div class="site-wrapper block_borders " >
-      <div class="site-wrapper-inner">
-        <div class="cover-container">
-     
-
-          <div class="inner cover " style="margin-top: 200px;">
-     
-        <div class="col-lg-4 text_block" style="height: 500px;" >
-
-<h1>Тьюторы, менторы <br>Школы Дизайна.<br>
-Design School Tutors, mentors.</h1>
-
-
-          </div>
-
-  
-       <?php
-
-   $course_programm_category = new WP_Query( $args );
-    $course_programm_category = new WP_Query( array(
+    <?php
+      $course_programm_category = new WP_Query( $args );
+      $course_programm_category = new WP_Query( array(
        'post_type' => 'team',
-      'paged'=>  $paged,
-      'order' => ASC,
+       'paged'=>  $paged,
+       'order' => ASC,
        'posts_per_page' => -1,
-  'tax_query' => array(
-   
-    array(
-      'taxonomy' => 'person_category',
-      'field'    => 'slug',
-      'terms'    => 'менторы-и-тьюторы-ux-design-essential-course',
-    ),
-  ),
-) );
-    if( $course_programm_category->have_posts() ) {
-      while( $course_programm_category->have_posts() ) {
-        $course_programm_category->the_post();
-        ?>
-        
+       'tax_query' => array(
+        array(
+          'taxonomy' => 'person_category',
+          'field'    => 'slug',
+          'terms'    => 'менторы-и-тьюторы-ux-design-essential-course',
+          ),
+        ),
+       ) );
+      if( $course_programm_category->have_posts() ) {
+        while( $course_programm_category->have_posts() ) {
+          $course_programm_category->the_post();
+          ?>
 
-   <div class="col-lg-4 course_item" >
-               <div class="col-lg-11 col-lg-offset-1">
-<div class="row item_border">
+          <div class="col-sm-6 col-md-4 col-xs-12 course_item" >
+           <div class="col-lg-11 col-lg-offset-right-1">
+            <div class="row item_border">
+              <a href="<?php echo get_post_meta($post->ID, 'person_soc_url', 1); ?>">                   
+                <div class="thumbnail_item">
+                  <?php the_post_thumbnail('','class=img-responsive'); ?>
+                </div>
 
-<a href="<?php echo get_post_meta($post->ID, 'person_soc_url', 1); ?>">                   <div class="thumbnail_item">
-<?php the_post_thumbnail('','class=img-responsive'); ?></div>
-
-<div class="col-lg-10" style="margin-left: 10px;">
-<h1><?php echo get_post_meta($post->ID, 'person_rus_name', 1); ?> </h1>
-<h1><?php echo get_post_meta($post->ID, 'person_eng_name', 1); ?> </h1>
-<p><?php echo get_post_meta($post->ID, 'person_jd', 1); ?> </p>
-</div>
-</a>
-
-</div>
-
-</div>
+                <div class="col-lg-10" style="margin-left: 10px;">
+                  <article>  
+                   <h1 style="margin-bottom: 0px;"><?php echo get_post_meta($post->ID, 'person_rus_name', 1); ?> </h1>
+                   <h1 style="margin-top: 0px;"><?php echo get_post_meta($post->ID, 'person_eng_name', 1); ?> </h1>
+                  </article>
+                  <div class="main1">
+                    <div id="main2" style="margin-bottom: 10px; ">
+                      <p><?php echo get_post_meta($post->ID, 'person_jd', 1); ?> </p>
+                    </div>
+                </div>
+              </div>
+            </a>
 
           </div>
+        </div>
+      </div>
 
-
-
-           
-       <?php
-      }
+      <?php
     }
-
-    else {
-     
-    }
+  }
+  else {
+  }
   ?>
   <?php wp_reset_postdata(); ?>
+</section>
 
-
-
-          </div>
+</div>
+<!-- 
 <button type="button" class="btn btn-grey">
-ПОСМОТРЕТЬ ВСЕХ ТЬЮТОРОВ, МЕНТОРОВ, СПИКЕРОВ
+  ПОСМОТРЕТЬ ВСЕХ ТЬЮТОРОВ, МЕНТОРОВ, СПИКЕРОВ
+</button> -->
+<!-- END OF BLOCK WITH TUTORS MENTORS OF DESIGN SCHOOL -->
 
-</button>
 
+<!-- BLOCK WITH TUTORS MENTORS OF BUSINESS SCHOOL -->
+<div class="col-xs-12 block_borders slide-styles" >
+  <section class="main2">
 
-        </div>
-
-      </div>
-
+    <div class="col-sm-6 col-md-4 col-xs-12  text_block">
+      <h1>Тьюторы, менторы<br> Школы Бизнеса.<br>
+        Business School <br>  Tutors, mentors.</h1>
     </div>
 
-
-
-<div class="site-wrapper block_borders " >
-      <div class="site-wrapper-inner">
-        <div class="cover-container">
-     
-
-          <div class="inner cover " style="margin-top: 200px;">
-     
-        <div class="col-lg-4 text_block" style="height: 500px;" >
-
-<h1>Тьюторы, менторы<br> Школы Бизнеса.<br>
-Business School <br>  Tutors, mentors.</h1>
-
-
-          </div>
-
-    
-       <?php
-
-   $course_programm_category = new WP_Query( $args );
-    $course_programm_category = new WP_Query( array(
+    <?php
+      $course_programm_category = new WP_Query( $args );
+      $course_programm_category = new WP_Query( array(
        'post_type' => 'team',
-      'paged'=>  $paged,
-      'order' => ASC,
+       'paged'=>  $paged,
+       'order' => ASC,
        'posts_per_page' => -1,
-  'tax_query' => array(
-   
-    array(
-      'taxonomy' => 'person_category',
-      'field'    => 'slug',
-      'terms'    => 'менторы-и-тьюторы-pm-online-course',
-    ),
-  ),
-) );
-    if( $course_programm_category->have_posts() ) {
-      while( $course_programm_category->have_posts() ) {
-        $course_programm_category->the_post();
-        ?>
-        
+       'tax_query' => array(
+        array(
+          'taxonomy' => 'person_category',
+          'field'    => 'slug',
+          'terms'    => 'менторы-и-тьюторы-pm-online-course',
+          ),
+        ),
+       ) );
+      if( $course_programm_category->have_posts() ) {
+        while( $course_programm_category->have_posts() ) {
+          $course_programm_category->the_post();
+          ?>
 
-   <div class="col-lg-4 course_item" >
-               <div class="col-lg-11 col-lg-offset-1">
-<div class="row item_border">
+          <div class="col-sm-6 col-md-4 col-xs-12  course_item" >
+           <div class="col-lg-11 col-lg-offset-right-1">
+            <div class="row item_border">
+              <a href="<?php echo get_post_meta($post->ID, 'person_soc_url', 1); ?>">                   
+                <div class="thumbnail_item">
+                  <?php the_post_thumbnail('','class=img-responsive'); ?>
+                </div>
 
-<a href="<?php echo get_post_meta($post->ID, 'person_soc_url', 1); ?>">                   <div class="thumbnail_item">
-<?php the_post_thumbnail('','class=img-responsive'); ?></div>
+                <div class="col-lg-10" style="margin-left: 10px;">
+                  <article>
+                    <h1 style="margin-bottom: 0px;"><?php echo get_post_meta($post->ID, 'person_rus_name', 1); ?> </h1>
+                    <h1 style="margin-top: 0px;"><?php echo get_post_meta($post->ID, 'person_eng_name', 1); ?> </h1>
+                  </article>
+                  <div class="main1">
+                    <div id="main2" style="margin-bottom: 10px; ">
+                      <p><?php echo get_post_meta($post->ID, 'person_jd', 1); ?> </p>
+                    </div>
+                  </div>
+                </div>
+              </a>
 
-<div class="col-lg-10" style="margin-left: 10px;">
-<h1><?php echo get_post_meta($post->ID, 'person_rus_name', 1); ?> </h1>
-<h1><?php echo get_post_meta($post->ID, 'person_eng_name', 1); ?> </h1>
-<p><?php echo get_post_meta($post->ID, 'person_jd', 1); ?> </p>
-</div>
-</a>
-
-</div>
-
-</div>
-
+            </div>
           </div>
+        </div>
 
-
-
-           
-       <?php
+        <?php
       }
     }
-
     else {
-     
     }
-  ?>
-  <?php wp_reset_postdata(); ?>
+    ?>
+    <?php wp_reset_postdata(); ?>
+  </section>
 
 
- 
+</div>
+<!--   <button type="button" class="btn btn-grey">
+   ПОСМОТРЕТЬ ВСЕХ ТЬЮТОРОВ, МЕНТОРОВ, СПИКЕРОВ
+  </button> -->
+
+<!-- END OF BLOCK WITH TUTORS MENTORS OF BUSINESS SCHOOL -->
 
 
-          </div>
-<button type="button" class="btn btn-grey">
-ПОСМОТРЕТЬ ВСЕХ ТЬЮТОРОВ, МЕНТОРОВ, СПИКЕРОВ
-
-</button>
-
-
-        </div>
-
-      </div>
-
-    </div>
 
 <!-- 
 
